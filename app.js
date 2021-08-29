d44d6948a0846f8944eba757a68342051332365e
Davidama <davidarteromartinez@gmail.com>
2021-08-29 17:24:04 +0200

    structure

---
+++ app.js
@@ -0,0 +1,10 @@
+//Servidor con Express
+const express = require ('express');
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+app.listen(3000,()=>{
+    console.log("Server UP running in http://localhost:3000");
+})
