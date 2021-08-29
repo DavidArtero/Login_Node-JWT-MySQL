15cc2a205871ac17091713a5c5a3629af98f22fe
Davidama <davidarteromartinez@gmail.com>
2021-08-29 19:21:36 +0200

    structure & router added

--- app.js
+++ app.js
@@ -5,6 +5,26 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
+
+//Llamar al router
+app.use('/', require('./routes/router'));
+
+//Setear Motor de plantilla
+app.set('view engine', 'ejs');
+
+//Setear carpeta public para archivos estáticos
+app.use(express.static('public'));
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+//Setear variables de entorno
+dotenv.config({path: './env/.env'});
+
+//Poder trabajar con cookies
+//app.use(cookieParser);
+
+
 app.listen(3000,()=>{
     console.log("Server UP running in http://localhost:3000");
-})
+});
